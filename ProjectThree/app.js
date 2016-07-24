4c618985140578b12571f27c2cd556f32aa8338d
Luke Erickson <dukealldaway84@gmail.com>
2016-07-24 12:28:22 -0500

    First Commit

---
+++ ProjectThree/app.js
@@ -0,0 +1,19 @@
+﻿//requires/imports the HTTP module
+var http = require('http');
+
+//Defines a port we want to listen to
+const PORT=8080;
+
+//Function which handles requests and send response
+function handleRequest(request, response) {
+    response.end('Hello Brian! ');
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Starts server
+server.listen(PORT, function () {
+    //Callback triggered when server is successfully listening.
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
